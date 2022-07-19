ca717bb768b798a086b07313ec11487a0cd0504d
Luca Maier <lumaier@ethz.ch>
2022-07-19 16:07:51 +0200

    implement next-closest encloser

--- plugin/bloomfile_nsec5/closest_test.go
+++ plugin/bloomfile_nsec5/closest_test.go
@@ -36,3 +36,29 @@ func TestClosestEncloser(t *testing.T) {
 		}
 	}
 }
+
+func TestNextClosestEncloser(t *testing.T) {
+	z, err := Parse(strings.NewReader(dbMiekNL), testzone, "stdin", 0)
+	if err != nil {
+		t.Fatalf("Expect no error when reading zone, got %q", err)
+	}
+
+	tests := []struct {
+		in, out string
+	}{
+		{"miek.nl.", "miek.nl."},
+		{"www.miek.nl.", "www.miek.nl."},
+
+		{"blaat.miek.nl.", "blaat.miek.nl."},
+		{"blaat.www.miek.nl.", "blaat.www.miek.nl."},
+		{"www.blaat.miek.nl.", "blaat.miek.nl."},
+		{"blaat.a.miek.nl.", "blaat.a.miek.nl."},
+	}
+
+	for _, tc := range tests {
+		nce := z.NextClosestEncloser(tc.in)
+		if nce != tc.out {
+			t.Errorf("Expected ce to be %s for %s, got %s", tc.out, tc.in, nce)
+		}
+	}
+}
