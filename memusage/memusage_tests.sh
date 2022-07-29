99985211f0784365db26aeff9203c90954c4bfa8
Luca Maier <lumaier@ethz.ch>
2022-07-29 10:00:03 +0200

    repeat measurements and nice printing

--- memusage/memusage_tests.sh
+++ memusage/memusage_tests.sh
@@ -7,18 +7,32 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
 filename="memusage_log".$current_time".txt"
 touch $filename
+filename="./memusage/"$filename
 
 cd ..
 
-filename="./memusage/"$filename
+temp_za="temp_za.txt"
+temp_ns="temp_ns.txt"
 
 function conduct() {
-    echo "" | tee -a $filename
-    echo "====================== Processing $2 =================================" | tee -a $filename
-    echo "" | tee -a $filename
-    echo "" | tee -a $filename
-    ./coredns -conf ./memusage/$1/$2 -dns.port 1054 | tee -a $filename
-    echo ""
+    ./coredns -conf ./memusage/$1/$2 -dns.port 1054 | tee -a $3
+}
+
+function repeat() {
+    touch $temp_za
+    touch $temp_ns
+    for j in {1..3}
+    do 
+        rm -f /memusage/testdata/db.miek.nl.signed
+        conduct "Corefiles_za" $1 $temp_za
+        conduct "Corefiles_ns" $2 $temp_ns
+    done
+    echo "================== Processing ZA $1 =================" | tee -a $filename
+    grep "TotalAlloc" $temp_za | tee -a $filename
+    echo "================== Processing NS $2 =================" | tee -a $filename
+    grep "TotalAlloc" $temp_ns | tee -a $filename
+    rm -f $temp_za
+    rm -f $temp_ns
 }
 
 # main function
@@ -29,14 +43,13 @@ do
     then
         for i in {1..3}
         do
-            rm -f /memusage/testdata/db.miek.nl.signed
             TEMP="${f}_v$i"
-            conduct "Corefiles_za" $TEMP
-            conduct "Corefiles_ns" $f
+            repeat $TEMP $f
         done
     else 
-        rm -f /memusage/testdata/db.miek.nl.signed
-        conduct "Corefiles_za" $f
-        conduct "Corefiles_ns" $f
+        repeat $f $f
     fi
 done
+
+
+
