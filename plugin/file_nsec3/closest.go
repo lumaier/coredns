b44dcd22c2d164d9340f687a7e1c15ca47d491b3
Luca Maier <lumaier@ethz.ch>
2022-08-15 07:37:49 +0200

    add nsec3

---
+++ plugin/file_nsec3/closest.go
@@ -0,0 +1,24 @@
+package file_nsec3
+
+import (
+	"github.com/coredns/coredns/plugin/file_nsec3/tree"
+
+	"github.com/miekg/dns"
+)
+
+// ClosestEncloser returns the closest encloser for qname.
+func (z *Zone) ClosestEncloser(qname string) (*tree.Elem, bool) {
+
+	offset, end := dns.NextLabel(qname, 0)
+	for !end {
+		elem, _ := z.Tree.Search(qname)
+		if elem != nil {
+			return elem, true
+		}
+		qname = qname[offset:]
+
+		offset, end = dns.NextLabel(qname, 0)
+	}
+
+	return z.Tree.Search(z.origin)
+}
---
+++ plugin/sign_nsec3/log_test.go
@@ -0,0 +1,5 @@
+package sign_nsec3
+
+import clog "github.com/coredns/coredns/plugin/pkg/log"
+
+func init() { clog.Discard() }
