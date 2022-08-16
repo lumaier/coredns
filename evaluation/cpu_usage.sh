1cd637134963dad742407e6e06c1773d62d313ac
Luca Maier <lumaier@ethz.ch>
2022-08-16 11:12:54 +0200

    cpu usage

--- evaluation/cpu_usage.sh
+++ evaluation/cpu_usage.sh
@@ -3,7 +3,7 @@ NS_IP=167.71.45.171
 PORT=1054
 TIME_RUN=120
 
-dnsperf -d ./testdata/queries/queries_nex -D -p $PORT -s $NS_IP -c $NR_CLIENTS -Q 32000 -l $TIME_RUN -v 
+dnsperf -d ./testdata/queries/queries_nex -D -p $PORT -s $NS_IP -c $NR_CLIENTS -Q 4000 -l $TIME_RUN -v 
 
 
 
