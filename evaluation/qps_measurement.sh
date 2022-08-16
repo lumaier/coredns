ff8f39c4b84491da5b1e3b8a56bda7d2a4c8290f
Luca Maier <lumaier@ethz.ch>
2022-08-16 10:41:38 +0200

    qps

--- evaluation/qps_measurement.sh
+++ evaluation/qps_measurement.sh
@@ -1,24 +1,9 @@
-MAX_QPS=64000
+MAX_QPS=96000
 NR_CLIENTS=24
 NS_IP=167.71.45.171
 # NS_IP=127.0.0.1
 PORT=1054
 TIME_RUN=10
-
-qps=(1000
-2000
-4000
-8000
-12000
-16000
-20000
-24000
-28000
-32000
-40000
-64000
-96000
-128000)
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 evaldir=./measurements/qps_measurement_$1_$current_time
@@ -28,7 +13,7 @@ temp=$evaldir"/log.txt"
 touch $temp
 
 echo "======================= $1 ===========================" | tee -a $temp
-for i in "${qps[@]}"; do
+for ((i=2000;i<=$MAX_QPS;i+=2000)); do
     dnsperf -d ./testdata/queries/queries_nex -D -p $PORT -s $NS_IP -c $NR_CLIENTS -Q $i -l $TIME_RUN -S 1 | tee -a $temp
 done
 
